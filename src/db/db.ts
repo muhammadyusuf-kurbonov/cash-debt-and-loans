dc3e2c8edc922ea077e6c6a9441993419bfeba42
Muhammadyusuf Kurbonov <muhammadyusuf.kurbonov2002@gmail.com>
2025-06-07 06:45:13 +0500

    populate initial data

--- src/db/db.ts
+++ src/db/db.ts
@@ -6,9 +6,3 @@ export const db = drizzle({
   connection: 'file:./db.sqlite',
   schema: schema,
 });
-
-if ((await db.select().from(schema.currencyTable)).length === 0) {
-  await db.insert(schema.currencyTable).values([
-    { name: 'So\'m', symbol: 'UZS' },
-  ]);
-}
---
+++ src/db/seed-initial.ts
@@ -0,0 +1,6 @@
+import { db } from "./db";
+import { currencyTable } from "./schema";
+
+await db.insert(currencyTable).values([
+    { name: 'So\'m', symbol: 'UZS' },
+]);
