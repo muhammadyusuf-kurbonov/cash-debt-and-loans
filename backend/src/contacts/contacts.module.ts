f37b6e0e469876639d560b9eb90484728f5b31fb
Muhammadyusuf Kurbonov <muhammadyusuf.kurbonov2002@gmail.com>
2025-10-28 22:00:00 +0500

    fix: import issue

--- backend/src/contacts/contacts.module.ts
+++ backend/src/contacts/contacts.module.ts
@@ -5,9 +5,10 @@ import { ContactsAttachService } from './contact-attach.service';
 import { ContactsController } from './contacts.controller';
 import { ContactsService } from './contacts.service';
 import { I18nService } from 'src/i18n/i18n.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [PrismaModule, TransactionsModule],
+  imports: [PrismaModule, TransactionsModule, UsersModule],
   controllers: [ContactsController],
   providers: [ContactsService, ContactsAttachService, I18nService],
   exports: [ContactsService, ContactsAttachService],
